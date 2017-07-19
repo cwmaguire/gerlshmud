1668b20250da6271dde287b70cff6b4ef2c1a62b
Chris Maguire <cwmaguire@gmail.com>
2017-07-19 15:43:17 -0700

    The PID macro is now used in play-testing code
    Since I'm using ?PID(foo) in the play-testing code I need to lift it up
    out of the test code and into production.

--- include/erlmud.hrl
+++ include/erlmud.hrl
@@ -1,14 +1,9 @@
+-define(PID(Value), {pid, Value}).
+
 -record(parents, {owner :: pid(),
                   character :: pid(),
                   top_item :: pid(),
                   body_part :: pid()}).
-
-%-record(attack, {source :: pid(),
-                 %target :: pid(),
-                 %attack_types = [] :: [atom()],
-                 %hit :: integer(),
-                 %damage :: integer(),
-                 %calc_type :: hit | damage | weight}).
 
 -record(top_item, {item :: pid(),
                    is_active :: boolean(),
