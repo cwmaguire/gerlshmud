e40c20186e36a908674515e19e4cd39787d1f248
Chris Maguire <cwmaguire@gmail.com>
2019-01-16 22:33:16 -0800

    More work on HTML+CSS log lines, adding SVG
    Adding SVG graphics to the log line to build arbitrary shapes on the fly
    instead of generating them as image files (e.g. JPG, PNG)

--- log_wrappers/erlmud.js
+++ log_wrappers/erlmud.js
@@ -6,24 +6,28 @@ for(let log of logs){
 
 function add_log_line(log){
   let logDiv = div();
-  let roomSpan = span(prop(log, 'room'), 'room');
+
+  let svg1 = svg_circle('#FFFFFF', '#000000');
+  logDiv.appendChild(svg1);
+
+  let roomSpan = span(prop(log, 'room', 'no room'), 'room');
 
   let roomTitleSpan = span('room', 'room_title');
 
   let sourceImage = img();
   let targetImage = img();
 
-  let logProcessChar1Span = span(prop(log, 'process'));
-  let logProcessChar2Span = span(prop(log, 'process'));
+  let logProcessChar1Span = span(prop(log, 'process', 'P'), 'log_process_1');
+  let logProcessChar2Span = span(prop(log, 'process', '?'), 'log_process_2');
 
-  let handlerSpan = span('handler: ' + prop(log, 'module'));
+  let handlerSpan = span(prop(log, 'module', 'no handler'));
 
   let eventParentSpan = span(log.type);
 
   let sourceSpan = span();
-  let sourceNameSpan = span(prop(log, 'name'));
-  let sourceProcChar1Span = span(prop(log, 'source'));
-  let sourceProcChar2Span = span(prop(log, 'source'));
+  let sourceNameSpan = span(prop(log, 'name', 'no name'));
+  let sourceProcChar1Span = span(prop(log, 'source', 'no source'));
+  let sourceProcChar2Span = span(prop(log, 'source', 'no source'));
 
   let eventNameSpan = span();
 
@@ -57,19 +61,24 @@ function add_log_line(log){
   logDiv.appendChild(eventParentSpan);
 
   document.body.appendChild(logDiv);
+
+  let plainDiv = div();
+  let logText = serialize(log);
+  plainDiv.innerText = logText;
+  document.body.appendChild(plainDiv);
 }
 
-function prop(log, key){
+function prop(log, key, def = ''){
   if(log.hasOwnProperty('props')){
-    for(let [key, value] of log.props){
-      if(key == key){
-        console.log('prop(log, ' + key + ') -> ' + value);
-        console.log(log.props);
-        return value;
+    for(let [k, v] of log.props){
+      if(k== key){
+        //console.log('prop(log, ' + k+ ') -> ' + v);
+        //console.log(log.props);
+        return v;
       }
     }
   }
-  return '';
+  return def;
 }
 
 function span(html, className){
@@ -89,4 +98,36 @@ function img(){
 
 function div(){
   return document.createElement('DIV');
+}
+
+function serialize(o){
+  let str = '';
+  for(let k in o){
+    str += k + ': ' + o[k] + ', ';
+  }
+  return str;
+}
+
+function svg_circle(fill = '#FFFFFF', stroke){
+  var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  var circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+
+  svg.setAttribute("aria-hidden","true");
+  svg.setAttribute('viewbox', '0 0 20 20');
+  //svg.setAttribute('width', '20px');
+  //svg.setAttribute('height', '20px');
+
+  circle.setAttribute('cx', '10');
+  circle.setAttribute('cy', '10');
+  circle.setAttribute('fill', fill);
+  if(stroke){
+    circle.setAttribute('r', '7');
+    circle.setAttribute('stroke', stroke);
+    circle.setAttribute('stroke-width', 2);
+  }else{
+    circle.setAttribute('r', '9');
+  }
+
+  svg.appendChild(circle);
+  return svg;
 }
