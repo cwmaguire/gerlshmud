8ac05ef80f1f9c9d158fd307bd153e02f06b23ed
Chris Maguire <cwmaguire@gmail.com>
2019-02-02 13:39:33 -0800

    Look up description templates with erlmud_config
    I have no idea why this wasn't already in the repository. The
    erlmud_config module returns, for a given property, how to describe it.
    Maybe I figured out some other way; more likely I just forgot.
    
    I've added a default description template because I ran into some
    objects that were missing. The default is just to display the name of
    the object (e.g. "dexterity") and whatever the value property is.

---
+++ src/erlmud_config.erl
@@ -0,0 +1,27 @@
+%% Copyright (c) 2017, Chris Maguire <cwmaguire@gmail.com>
+%%
+%% Permission to use, copy, modify, and/or distribute this software for any
+%% purpose with or without fee is hereby granted, provided that the above
+%% copyright notice and this permission notice appear in all copies.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+%% ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+%% OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+-module(erlmud_config).
+
+-include("include/erlmud.hrl").
+
+-export([desc_template/1]).
+
+desc_template(Type) ->
+    DefaultTemplate = default_template(Type),
+    DescTemplates = application:get_env(erlmud, desc_templates, []),
+    proplists:get_value(Type, DescTemplates, DefaultTemplate).
+
+default_template(Type) ->
+    Text = atom_to_binary(Type, utf8),
+    [<<Text/binary, ": ">>, value].
