27a3426f901798f16183938396eef81a070e9a83
Chris Maguire <cwmaguire@gmail.com>
2019-02-02 13:21:34 -0800

    Give objects icons for logging and fix connections
    For logging I like to give each object an icon so it's easy to see what
    source and target each log entry involves.
    
    Some of the play world connections were broken. I don't know where I got
    the data from but the objects weren't connected properly.

--- include/play_world.hrl
+++ include/play_world.hrl
@@ -23,31 +23,38 @@
                             {item, force_field},
                             {name, <<"room">>},
                             {desc, <<"an empty space">>},
+                            {icon, room},
                             {exit, exit_5_5_and_5_4},
                             ?ROOM_HANDLERS]},
 
                 {room_5_4, [{exit, exit_5_5_and_5_4},
+                            {icon, room},
                             ?ROOM_HANDLERS]},
 
                 {room_5_3, [{exit, exit_5_4_and_5_3},
+                            {icon, room},
                             ?ROOM_HANDLERS]},
 
                 {room_6_5, [{exit, exit_5_5_and_6_5},
+                            {icon, room},
                             ?ROOM_HANDLERS]},
 
                 {exit_5_5_and_5_4,
                            [{{room, n}, room_5_4},
                             {{room, s}, room_5_5},
+                            {icon, exit},
                             ?EXIT_HANDLERS]},
 
                 {exit_5_4_and_5_3,
                            [{{room, n}, room_5_3},
                             {{room, s}, room_5_4},
+                            {icon, exit},
                             ?EXIT_HANDLERS]},
 
                 {exit_5_5_and_6_5,
                            [{{room, e}, room_6_5},
                             {{room, w}, room_5_5},
+                            {icon, exit},
                             ?EXIT_HANDLERS]},
 
                 {player, [{name, <<"Bob">>},
@@ -62,14 +69,17 @@
                           {body_part, hand0},
                           {body_part, hand1},
                           {race, race0},
+                          {icon, person},
                           ?CHARACTER_HANDLERS]},
 
                 {p_hp, [{hitpoints, 10},
                         {owner, player},
+                        {icon, stat},
                         ?HITPOINTS_HANDLERS]},
 
                 {p_life, [{is_alive, true},
                           {owner, player},
+                          {icon, stat},
                           ?LIFE_HANDLERS]},
 
                 {force_field, [{owner, player},
@@ -79,6 +89,7 @@
                                {desc, [name]},
                                {defence_damage_modifier, 100},
                                {is_defence, true},
+                               {icon, technology},
                                ?ITEM_HANDLERS]},
 
                 {sword, [{desc, "sword"},
@@ -90,6 +101,7 @@
                          {defence_hit_modifier, 0},
                          {attack_damage_modifier, 20},
                          {is_defence, true},
+                         {icon, weapon},
                          ?ITEM_HANDLERS]},
 
                 {shield, [{owner, player},
@@ -99,6 +111,7 @@
                           {desc, [name]},
                           {defence_hit_modifier, 100},
                           {is_defence, true},
+                         {icon, weapon},
                           ?ITEM_HANDLERS]},
 
                 {strength0, [{owner, player},
@@ -106,6 +119,7 @@
                              {value, 17},
                              {attack_damage_modifier, 100},
                              {desc, [<<"strength ">>, value]},
+                             {icon, stat},
                              ?ATTRIBUTE_HANDLERS]},
 
                 {dexterity0, [{owner, player},
@@ -113,6 +127,7 @@
                               {value, 15},
                               {attack_hit_modifier, 100},
                               {desc, [<<"dexterity ">>, value]},
+                              {icon, stat},
                               ?ATTRIBUTE_HANDLERS]},
 
                 {p_stamina, [{owner, player},
@@ -120,6 +135,7 @@
                              {per_tick, 1},
                              {tick_time, 10},
                              {max, 10},
+                             {icon, stat},
                              ?RESOURCE_HANDLERS]},
 
                 {hand0,   [{name, <<"left hand">>},
@@ -127,12 +143,14 @@
                            {body_part, hand},
                            {max_items, 1},
                            {item, p_fist},
+                           {icon, body_part},
                            ?BODY_PART_HANDLERS]},
 
                 {hand1,   [{name, <<"right hand">>},
                            {owner, player},
                            {body_part, hand},
                            {max_items, 1},
+                           {icon, body_part},
                            ?BODY_PART_HANDLERS]},
 
                 {p_fist, [{name, <<"left fist">>},
@@ -145,15 +163,17 @@
                           {is_attack, true},
                           {is_auto_attack, true},
                           {resources, [{stamina, 5}]},
+                          {icon, body_part},
                           ?ITEM_HANDLERS]},
 
                 {p_back,   [{name, <<"back">>},
                            {owner, player},
                            {body_part, back},
                            {max_items, 2},
+                           {icon, body_part},
                            ?BODY_PART_HANDLERS]},
 
-                {giant, [{owner, room1},
+                {giant, [{owner, room_5_4},
                          {name, <<"Pete">>},
                          {hitpoints, g_hp},
                          {life, g_life},
@@ -162,20 +182,24 @@
                          {attribute, dexterity1},
                          {attribute, race},
                          {stamina, g_stamina},
+                         {icon, person},
                          ?CHARACTER_HANDLERS]},
 
                 {g_hp, [{hitpoints, 310},
                         {owner, giant},
+                        {icon, stat},
                         ?HITPOINTS_HANDLERS]},
 
                 {g_life, [{is_alive, true},
                           {owner, giant},
+                          {icon, stat},
                           ?LIFE_HANDLERS]},
 
                 {g_hand_r, [{name, <<"right hand">>},
                             {owner, giant},
                             {body_part, hand},
                             {item, g_club},
+                            {icon, body_part},
                             ?BODY_PART_HANDLERS]},
 
                 {strength1, [{owner, giant},
@@ -183,6 +207,7 @@
                              {value, 17},
                              {attack_damage_modifier, 50},
                              {desc, [<<"strength ">>, value]},
+                             {icon, stat},
                              ?ATTRIBUTE_HANDLERS]},
 
                 {dexterity1, [{owner, player},
@@ -191,6 +216,7 @@
                               {attack_hit_modifier, 50},
                               {defence_hit_modifier, 50},
                               {desc, [<<"dexterity ">>, value]},
+                              {icon, stat},
                               ?ATTRIBUTE_HANDLERS]},
 
                 {g_stamina, [{owner, giant},
@@ -198,11 +224,13 @@
                              {per_tick, 1},
                              {tick_time, 10},
                              {max, 10},
+                             {icon, stat},
                              ?RESOURCE_HANDLERS]},
 
                 {race0, [{owner, giant},
                          {defence_damage_modifier, 50},
                          {desc, [<<"giant">>]},
+                         {icon, stat},
                          ?ATTRIBUTE_HANDLERS]},
 
                 {g_club, [{name, <<"giant club">>},
@@ -215,5 +243,6 @@
                           {is_attack, true},
                           {is_auto_attack, true},
                           {resources, [{stamina, 5}]},
+                          {icon, weapon},
                           ?ITEM_HANDLERS]}
                  ]).
