15b122102cf715d9b5d35bf7e0c3be63ec9ed66f
Chris Maguire <cwmaguire@gmail.com>
2021-02-20 17:14:21 -0800

    Add universal handler to delete properties
    If I'm going to create effect processes to apply effects then I need to
    delete them as well.
    
    This means ?UNIVERSAL_HANDLERS is now a list, not a single atom.

--- include/gerlshmud_handlers.hrl
+++ include/gerlshmud_handlers.hrl
@@ -1,9 +1,10 @@
--define(UNIVERSAL_HANDLERS, gerlshmud_handler_set_child_property).
+-define(UNIVERSAL_HANDLERS, [gerlshmud_handler_set_child_property,
+                             gerlshmud_handler_delete_property]).
 
 -define(ROOM_HANDLERS, {handlers, [gerlshmud_handler_room_inject_self,
                                    gerlshmud_handler_room_inv,
                                    gerlshmud_handler_room_look,
-                                   gerlshmud_handler_room_move,
+                                   gerlshmud_handler_room_move |
                                    ?UNIVERSAL_HANDLERS]}).
 
 -define(CHARACTER_HANDLERS, {handlers, [gerlshmud_handler_char_attack,
@@ -11,54 +12,54 @@
                                         gerlshmud_handler_char_inv,
                                         gerlshmud_handler_char_move,
                                         gerlshmud_handler_char_inject_self,
-                                        gerlshmud_handler_char_enter_world,
+                                        gerlshmud_handler_char_enter_world |
                                         ?UNIVERSAL_HANDLERS]}).
 
 -define(ITEM_HANDLERS, {handlers, [gerlshmud_handler_item_look,
                                    gerlshmud_handler_item_inv,
-                                   gerlshmud_handler_item_inject_self,
+                                   gerlshmud_handler_item_inject_self |
                                    ?UNIVERSAL_HANDLERS]}).
 
 -define(CONN_HANDLERS, {handlers, [gerlshmud_handler_conn_enter_world,
                                    gerlshmud_handler_conn_move,
-                                   gerlshmud_handler_conn_send,
+                                   gerlshmud_handler_conn_send |
                                    ?UNIVERSAL_HANDLERS]}).
 
 -define(BODY_PART_HANDLERS, {handlers, [gerlshmud_handler_body_part_look,
                                         gerlshmud_handler_body_part_inv,
-                                        gerlshmud_handler_body_part_inject_self,
+                                        gerlshmud_handler_body_part_inject_self |
                                         ?UNIVERSAL_HANDLERS]}).
 
 -define(ATTRIBUTE_HANDLERS, {handlers, [gerlshmud_handler_attribute_look,
-                                        gerlshmud_handler_attribute_attack,
+                                        gerlshmud_handler_attribute_attack |
                                         ?UNIVERSAL_HANDLERS]}).
 
 -define(EXIT_HANDLERS, {handlers, [gerlshmud_handler_exit_move,
-                                   gerlshmud_handler_exit_look,
+                                   gerlshmud_handler_exit_look |
                                    ?UNIVERSAL_HANDLERS]}).
 
--define(HITPOINTS_HANDLERS, {handlers, [gerlshmud_handler_hitpoints_attack,
+-define(HITPOINTS_HANDLERS, {handlers, [gerlshmud_handler_hitpoints_attack |
                                         ?UNIVERSAL_HANDLERS]}).
 
--define(LIFE_HANDLERS, {handlers, [gerlshmud_handler_life_attack,
+-define(LIFE_HANDLERS, {handlers, [gerlshmud_handler_life_attack |
                                    ?UNIVERSAL_HANDLERS]}).
 
--define(STAT_HANDLERS, {handlers, [gerlshmud_handler_stat_look,
+-define(STAT_HANDLERS, {handlers, [gerlshmud_handler_stat_look |
                                    ?UNIVERSAL_HANDLERS]}).
 
 -define(RESOURCE_HANDLERS, {handlers, [gerlshmud_handler_resource_inject_self,
                                        gerlshmud_handler_resource_tick,
-                                       gerlshmud_handler_resource_reserve,
+                                       gerlshmud_handler_resource_reserve |
                                        ?UNIVERSAL_HANDLERS]}).
 
 -define(SPELL_HANDLERS, {handlers, [gerlshmud_handler_attack,
                                     gerlshmud_handler_spell_memorize,
-                                    gerlshmud_handler_spell_inject_self,
+                                    gerlshmud_handler_spell_inject_self |
                                     ?UNIVERSAL_HANDLERS]}).
 
 -define(EFFECT_PROTOTYPE_HANDLERS, {handlers, [gerlshmud_handler_effect_create]}).
 
--define(EFFECT_HANDLERS, {handlers, [gerlshmud_handler_effect_attack,
+-define(EFFECT_HANDLERS, {handlers, [gerlshmud_handler_effect_attack |
                                      ?UNIVERSAL_HANDLERS]}).
 
 -define(WEAPON_HANDLERS, {handlers, [gerlshmud_handler_attack |
---
+++ src/handlers/gerlshmud_handler_delete_property.erl
@@ -0,0 +1,38 @@
+%% Copyright (c) 2021, Chris Maguire <cwmaguire@protonmail.com>
+%%
+%% Permission to use, copy, modify, and/or distribute this software for any
+%% purpose with or without fee is hereby granted, provided that the above
+%% copyright notice and this permission notice appear in all copies.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+%% ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+%% OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+-module(gerlshmud_handler_delete_property).
+-behaviour(gerlshmud_handler).
+
+-include("include/gerlshmud.hrl").
+
+%% object behaviour
+-export([attempt/1]).
+-export([succeed/1]).
+-export([fail/1]).
+
+attempt({#parents{},
+         Props,
+         {delete, Pid}}) ->
+    Log = [{?SOURCE, Pid},
+           {?EVENT, delete}],
+    Props2 = lists:keydelete(Pid, 2, Props),
+    {succeed, false, Props2, Log};
+attempt(_) ->
+    undefined.
+
+succeed({Props, _Msg}) ->
+    Props.
+
+fail({Props, _, _}) ->
+    Props.
